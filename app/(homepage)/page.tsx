f27b5d6ef9f78208b5a4d707d084fb0b819c9139
Manish Prajapati <manishprajapatim416@gmail.com>
2023-08-25 21:16:18 +0530

    Used newly created user data type

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -2,10 +2,17 @@ import { getUserCollection } from "@/lib/mongodb";
 
 export default async function Home() {
   const collection = await getUserCollection();
-  const data = await collection.findOne({});
+  const data = (await collection.findOne({})) as userDataType | null;
   return (
     <main>
-      <div className="">{data?.name}</div>
+      <div className="">
+        {data && (
+          <div>
+            <h1>{data.name}</h1>
+            <h2>{data.email}</h2>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
