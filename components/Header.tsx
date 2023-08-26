1a369be51c21d68424b41dcd1896ba089eb3c013
Manish Prajapati <manishprajapatim416@gmail.com>
2023-08-26 17:52:40 +0530

    Changed backdrop to lg

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {}
 const Header: FC<HeaderProps> = ({}) => {
   return (
     <>
-      <header className="h-16 backdrop-blur-md fixed top-0 left-0 right-0 shadow-sm shadow-neutral-500">
+      <header className="h-16 backdrop-blur-lg fixed top-0 left-0 right-0 shadow-sm shadow-neutral-500">
         <div className="container mx-auto h-full flex flex-row justify-between items-center px-3">
           {/* Logo */}
           <div className="flex flex-row items-center cursor-pointer h-full">
