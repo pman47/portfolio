66d1f45908ae3bdfdfbfa2c4cad6ae6832d3eefb
Manish Prajapati <manishprajapatim416@gmail.com>
2023-08-26 23:27:30 +0530

    Added basic introduction section

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,13 +1,25 @@
 import BackgroundImage from "@/components/BackgroundImage";
+import HeroSection from "@/components/HeroSection";
+import { Suspense } from "react";
 import UserDetails from "./components/UserDetails";
+
+import { Bricolage_Grotesque } from "next/font/google";
+const bricolage_Grotesque = Bricolage_Grotesque({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default async function Home() {
   return (
-    <main>
+    <main className={bricolage_Grotesque.className}>
       <BackgroundImage />
       <div className="customContainer mx-auto">
+        <HeroSection />
         {/* Basic User Details Component */}
-        <UserDetails />
+        <Suspense fallback={<div>Loading...</div>}>
+          {/* @ts-expect-error */}
+          <UserDetails />
+        </Suspense>
 
         {/* Projects */}
 
