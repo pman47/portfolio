7d3b70f397f9d5890d0ef7c133c0ed03f15561b5
Manish Prajapati <manishprajapatim416@gmail.com>
2023-08-27 23:58:18 +0530

    Added favicon

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    favicon: "/images/favicon.png",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
