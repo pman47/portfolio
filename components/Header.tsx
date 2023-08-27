1bc00aa2875be8d072a23bf83850a19a2c6e4899
Manish Prajapati <manishprajapatim416@gmail.com>
2023-08-27 23:00:23 +0530

    Changed stylings

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,15 +11,14 @@ const Header: FC<HeaderProps> = ({}) => {
       <header className="h-16 backdrop-blur-lg fixed top-0 left-0 right-0 shadow-sm shadow-neutral-500 z-10">
         <div className="customContainer mx-auto h-full flex flex-row justify-between items-center px-3">
           {/* Logo */}
-          <div className="flex flex-row items-center cursor-pointer h-full">
-            {/* <Ghost size={20} /> */}
+          <div className="glassmorphism cursor-pointer ms-1 px-3 py-2">
             <span className={`text-3xl ${lobster.className}`}>M</span>
           </div>
 
           {/* Action Links */}
         </div>
       </header>
-      <div className="h-16 w-full bg-neutral-800" />
+      <div className="h-16 w-full bg-neutral-900" />
     </>
   );
 };
