1ba2b7de5f59b91d1c51252f3b7c9229b8c85b21
Manish Prajapati <manishprajapatim416@gmail.com>
2023-09-02 16:34:40 +0530

    Added small bg color with opacity

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {}
 const Header: FC<HeaderProps> = ({}) => {
   return (
     <>
-      <header className="h-16 backdrop-blur-lg fixed top-0 left-0 right-0 shadow-sm shadow-neutral-500 z-10">
+      <header className="h-16 backdrop-blur-sm fixed top-0 left-0 right-0 shadow-sm shadow-neutral-700 z-50 bg-neutral-900/30">
         <div className="customContainer mx-auto h-full flex flex-row justify-between items-center px-3">
           {/* Logo */}
           <div className="glassmorphism cursor-pointer h-12 ms-1 px-3">
